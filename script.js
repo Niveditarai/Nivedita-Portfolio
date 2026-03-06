e6aeb2dc0e699eca0ed6199d206c45b34b4543aa
Your Name <niveditarai095@gmail.com>
2026-03-06 22:11:34 +0530

    added typing animation and project buttons

---
+++ script.js
@@ -0,0 +1,11 @@
+var typed = new Typed("#typing", {
+strings: [
+"Machine Learning Enthusiast",
+"Python Developer",
+"Data Analyst",
+"Web Developer"
+],
+typeSpeed: 60,
+backSpeed: 40,
+loop: true
+});
